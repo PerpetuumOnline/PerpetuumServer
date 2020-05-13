cfd5dd7864d4cb34bea981fe9ad10b9e6c7dd20d
mikejeffers <mike.s.jeffers@gmail.com>
2020-05-13 18:53:18 -0400

    refactor OreNPCSpawner class

--- src/Perpetuum/Services/EventServices/EventProcessors/OreNPCSpawner.cs
+++ src/Perpetuum/Services/EventServices/EventProcessors/OreNPCSpawner.cs
@@ -15,12 +15,13 @@ using System.Threading.Tasks;
 namespace Perpetuum.Services.EventServices.EventProcessors
 {
     /// <summary>
-    /// EventListener for each zone, receives messages for mineralnode mined
-    /// TODO need to track foreach node: is there a current npc, is there a new npc to be spawned
+    /// EventListener for each zone, receives messages for mineralnode mined and spawns npc presence based on the INpcReinforcementsRepository configurations
     /// </summary>
     public class OreNpcSpawner : EventProcessor<EventMessage>
     {
         private const int SPAWN_DIST_FROM_FIELD = 50;
+        private readonly TimeSpan ORE_SPAWN_LIFETIME = TimeSpan.FromHours(3);
+        private readonly TimeSpan SPAWN_DELAY = TimeSpan.FromSeconds(10);
 
         private readonly IZone _zone;
         private readonly IDictionary<MineralNode, DynamicPresence> spawnedPresences = new Dictionary<MineralNode, DynamicPresence>();
@@ -57,7 +58,7 @@ namespace Perpetuum.Services.EventServices.EventProcessors
             }
         }
 
-        private Position FindSpawnLocation(Position start, double range)
+        private Position TryFindSpawnLocation(Position start, double range)
         {
             var p = new Position();
             List<Point> result = null;
@@ -88,6 +89,64 @@ namespace Perpetuum.Services.EventServices.EventProcessors
             return percent;
         }
 
+        private void CheckMineralNodeReinforcements(OreNpcSpawnMessage msg)
+        {
+            var node = msg.GetMineralNode();
+            if (!reinforcementsByMineralNode.ContainsKey(node))
+            {
+                var oreSpawn = _npcReinforcementsRepo.CreateOreNPCSpawn(node.Type);
+                reinforcementsByMineralNode.Add(node, oreSpawn);
+            }
+        }
+
+        private bool IsNodeDead(OreNpcSpawnMessage msg)
+        {
+            if (msg.GetOreNodeState() == OreNodeState.Removed)
+            {
+                RemoveEntry(msg.GetMineralNode());
+                return true;
+            }
+            return false;
+        }
+
+        private bool AlreadyHasActivePresence(OreNpcSpawnMessage msg)
+        {
+            return spawnedPresences.ContainsKey(msg.GetMineralNode());
+        }
+
+        private Position FindSpawnPosition(OreNpcSpawnMessage msg)
+        {
+            var node = msg.GetMineralNode();
+            var fieldCenter = node.Area.Center.ToPosition();
+            return TryFindSpawnLocation(fieldCenter, SPAWN_DIST_FROM_FIELD);
+        }
+
+        private INpcReinforcementWave GetNextWave(OreNpcSpawnMessage msg)
+        {
+            var node = msg.GetMineralNode();
+            var percent = ComputeFieldPercentConsumed(node);
+            return reinforcementsByMineralNode[node].GetNextPresence(percent);
+        }
+
+        private Position GetOreFieldCenter(OreNpcSpawnMessage msg)
+        {
+            return msg.GetMineralNode().Area.Center.ToPosition();
+        }
+
+        private void DoBeams(Position beamLocation)
+        {
+            _zone.CreateBeam(BeamType.npc_egg_beam, b => b.WithPosition(beamLocation).WithDuration(SPAWN_DELAY));
+            _zone.CreateBeam(BeamType.teleport_storm, b => b.WithPosition(beamLocation).WithDuration(SPAWN_DELAY));
+        }
+
+        private void DoSpawning(INpcReinforcementWave wave, Position homePosition, Position spawnPosition, MineralNode node)
+        {
+            var pres = _zone.AddDynamicPresenceToPosition(wave.Presence, homePosition, spawnPosition, ORE_SPAWN_LIFETIME);
+            wave.Spawned = true;
+            pres.PresenceExpired += OnPresenceExpired;
+            spawnedPresences.Add(node, pres);
+        }
+
         private bool _spawning = false;
 
         public override void OnNext(EventMessage value)
@@ -95,51 +154,35 @@ namespace Perpetuum.Services.EventServices.EventProcessors
             if (value is OreNpcSpawnMessage msg && _zone.Id == msg.GetZoneID())
             {
                 if (_spawning)
-                {
                     return;
-                }
-                var node = msg.GetMineralNode();
-                if (!reinforcementsByMineralNode.ContainsKey(node))
-                {
-                    var oreSpawn = _npcReinforcementsRepo.CreateOreNPCSpawn(node.Type);
-                    reinforcementsByMineralNode.Add(node, oreSpawn);
-                }
-                Logger.Info(reinforcementsByMineralNode[node].ToString());
-                if (spawnedPresences.ContainsKey(node))
-                {
-                    if (msg.GetOreNodeState() == OreNodeState.Removed)
-                    {
-                        //Node has been removed from zone - remove from our cache
-                        RemoveEntry(node);
-                    }
-                    //There is an active presence
+
+                CheckMineralNodeReinforcements(msg);
+
+                if (IsNodeDead(msg))
                     return;
-                }
-                var fieldCenter = node.Area.Center.ToPosition();
-                var spawnPos = FindSpawnLocation(fieldCenter, SPAWN_DIST_FROM_FIELD);
+
+                if (AlreadyHasActivePresence(msg))
+                    return;
+
+                var spawnPos = FindSpawnPosition(msg);
                 if (spawnPos == Position.Empty)
-                {
                     return; // Failed to find valid spawn location, try again on next cycle
-                }
-                var percent = ComputeFieldPercentConsumed(node);
-                var orePresence = reinforcementsByMineralNode[node].GetNextPresence(percent);
-                if (orePresence == null)
-                {
+
+                var wave = GetNextWave(msg);
+                if (wave == null)
                     return; // Presence already spawned once, or not found
-                }
-                var delay = TimeSpan.FromSeconds(10);
-                _zone.CreateBeam(BeamType.npc_egg_beam, b => b.WithPosition(fieldCenter).WithDuration(delay));
-                _zone.CreateBeam(BeamType.teleport_storm, b => b.WithPosition(fieldCenter).WithDuration(delay));
+
+                var node = msg.GetMineralNode();
+                var fieldCenter = GetOreFieldCenter(msg);
+
+                DoBeams(fieldCenter);
 
                 _spawning = true;
-                Task.Delay(delay).ContinueWith(t =>
+                Task.Delay(SPAWN_DELAY).ContinueWith(t =>
                 {
                     try
                     {
-                        var pres = _zone.AddDynamicPresenceToPosition(orePresence.Presence, fieldCenter, spawnPos, TimeSpan.FromSeconds(180)); // TODO timeout to 3 hr
-                        orePresence.Spawned = true;
-                        pres.PresenceExpired += OnPresenceExpired;
-                        spawnedPresences.Add(node, pres);
+                        DoSpawning(wave, fieldCenter, spawnPos, node);
                     }
                     catch (Exception ex)
                     {
