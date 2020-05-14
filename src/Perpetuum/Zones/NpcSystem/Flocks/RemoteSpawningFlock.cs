ed54c31bbf0f7fb17c20dfc365063d3ac5887d3f
mikejeffers <mike.s.jeffers@gmail.com>
2020-05-13 23:33:45 -0400

    normalflock spawns incrementally, which we dont want

--- src/Perpetuum/Zones/NpcSystem/Flocks/RemoteSpawningFlock.cs
+++ src/Perpetuum/Zones/NpcSystem/Flocks/RemoteSpawningFlock.cs
@@ -5,7 +5,7 @@ namespace Perpetuum.Zones.NpcSystem.Flocks
     /// <summary>
     /// This Flock is to be used with the DynamicePresenceExtended class to take advantage of a settable spawn location separate from its HomePosition
     /// </summary>
-    public class RemoteSpawningFlock : NormalFlock
+    public class RemoteSpawningFlock : Flock
     {
         public RemoteSpawningFlock(IFlockConfiguration configuration, Presence presence) : base(configuration, presence)
         {
